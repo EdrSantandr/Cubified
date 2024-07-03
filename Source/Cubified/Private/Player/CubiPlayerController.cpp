274f9357bb9aca54c61b51bf4ec250bf190422da
Eder Santander <eder.santander.123@gmail.com>
2024-07-02 19:51:43 -0500

    Added mapping context and inputmode for cursor

--- Source/Cubified/Private/Player/CubiPlayerController.cpp
+++ Source/Cubified/Private/Player/CubiPlayerController.cpp
@@ -2,4 +2,24 @@
 
 
 #include "Player/CubiPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+ACubiPlayerController::ACubiPlayerController()
+{
+	bReplicates = true;
+}
+
+void ACubiPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(CubiContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(CubiContext, 0);
+	bShowMouseCursor = false;
+	FInputModeGameAndUI InputModeGameAndUI;
+	InputModeGameAndUI.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeGameAndUI.SetHideCursorDuringCapture(true);
+	SetInputMode(InputModeGameAndUI);
+}
--- Source/Cubified/Public/Player/CubiPlayerController.h
+++ Source/Cubified/Public/Player/CubiPlayerController.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerController.h"
 #include "CubiPlayerController.generated.h"
 
+class UInputMappingContext;
 /**
  * 
  */
@@ -13,5 +14,15 @@ UCLASS()
 class CUBIFIED_API ACubiPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	ACubiPlayerController();
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> CubiContext;
 	
 };
